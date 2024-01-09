f9114d7c874f61eff5db2f49c25a35593cd6e1ee
Wiibleyde <nathan@bonnell.fr>
2024-01-09 16:31:17 +0100

    Update Melon prefab and DropFruit script
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- Assets/Scripts/DropFruit.cs
+++ Assets/Scripts/DropFruit.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class DropFruit : MonoBehaviour
 {
+    GameObject fruit;
     // Update is called once per frame
     void Update()
     {
+        string[] possibility = { "Cherry", "Strawberry", "Grape", "Persimmons", "Orange" };
         // Drop the fruit when the space bar is pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log("Space bar pressed");
             // Get the position of the player
             Vector3 playerPosition = transform.position;
+            Debug.Log("Player position: " + playerPosition);
             // Create a new position for the fruit
-            Vector3 fruitPosition = new Vector3(playerPosition.x, playerPosition.y + 0.5f, playerPosition.z);
-            // Create a new fruit tagged as "Cherry"
-            GameObject fruit = Instantiate(Resources.Load("Cherry"), fruitPosition, Quaternion.identity) as GameObject;
+            Vector3 fruitPosition = new Vector3(playerPosition.x, playerPosition.y - 0.5f, playerPosition.z);
+            Debug.Log("Fruit position: " + fruitPosition);
+            // Create a new fruit randomly from the name of the prefab ("Cherry""Strawberry""Grape""Persimmons""Orange") 
+            fruit = Instantiate(Resources.Load(possibility[Random.Range(0, possibility.Length)]) as GameObject, fruitPosition, Quaternion.identity);
             // Add a force to the fruit
             fruit.GetComponent<Rigidbody>().AddForce(Vector3.up * 100);
         }
