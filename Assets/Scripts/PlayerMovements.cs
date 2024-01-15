87b7dde16a8e023d5eaa0e21f4c036a740cc6e7f
Wiibleyde <nathan@bonnell.fr>
2024-01-15 17:40:08 +0100

    Update player movement and add Linux toolchain package
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -11,6 +11,10 @@ public class PlayerMovements : MonoBehaviour
     {
 		// Get the horizontal inputs. and limit the position of the player to the screen
 		float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-		transform.position = new Vector3(Mathf.Clamp(transform.position.x + horizontal, -2.5f, 2.5f), transform.position.y, transform.position.z);
+		// Get current fruit size and limit the position of the player to the screen
+		GameObject currentFruit = transform.GetChild(1).gameObject;
+		float currentFruitSize = currentFruit.GetComponent<SpriteRenderer>().bounds.size.x;
+		Debug.Log(currentFruitSize);
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x + horizontal, -2.5f + currentFruitSize / 60.0f, 2.5f - currentFruitSize / 60.0f), transform.position.y, transform.position.z);
     }
 }
