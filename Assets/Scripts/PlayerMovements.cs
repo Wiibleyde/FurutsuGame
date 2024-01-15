cde43e17f77377a17bbbefe817f855cf68afcd0e
Wiibleyde <nathan@bonnell.fr>
2024-01-15 17:46:46 +0100

    Update player movement clamp values
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -15,6 +15,6 @@ public class PlayerMovements : MonoBehaviour
 		GameObject currentFruit = transform.GetChild(1).gameObject;
 		float currentFruitSize = currentFruit.GetComponent<SpriteRenderer>().bounds.size.x;
 		Debug.Log(currentFruitSize);
-		transform.position = new Vector3(Mathf.Clamp(transform.position.x + horizontal, -2.5f + currentFruitSize / 60.0f, 2.5f - currentFruitSize / 60.0f), transform.position.y, transform.position.z);
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x + horizontal, -2.5f + currentFruitSize / 50.0f, 2.5f - currentFruitSize / 50.0f), transform.position.y, transform.position.z);
     }
 }
