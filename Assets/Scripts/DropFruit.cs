b34b8282cb9a0e522235db7db472f320dc925432
Wiibleyde <nathan@bonnell.fr>
2024-01-15 11:58:36 +0100

    Update fruit sorting order
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- Assets/Scripts/DropFruit.cs
+++ Assets/Scripts/DropFruit.cs
@@ -6,12 +6,53 @@ using random = UnityEngine.Random;
 public class DropFruit : MonoBehaviour
 {
     [SerializeField] public GameObject[] fruitsPrefabs;
+    [SerializeField] public GameObject player; // Ajoutez cette ligne
+    private GameObject nextFruit;
+    bool isCooldown = false;  
+
+    void Start()
+    {
+        PrepareNextFruit();
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isCooldown)
         {
-            GameObject nextFruit = Instantiate(fruitsPrefabs[random.Range(0, 5)]);
-            nextFruit.transform.position = transform.position;
+            StartCoroutine(Cooldown());
+            DropCurrentFruit();
+            PrepareNextFruit();
         }
     }
+
+    void PrepareNextFruit()
+    {
+        nextFruit = Instantiate(fruitsPrefabs[random.Range(0, 5)]);
+        nextFruit.transform.position = transform.position;
+        // Make the fruit shown in the UI follow the player but not collide with it
+        nextFruit.GetComponent<Rigidbody2D>().isKinematic = true;
+        nextFruit.GetComponent<Rigidbody2D>().simulated = false;
+        nextFruit.GetComponent<Collider2D>().enabled = false;
+        nextFruit.GetComponent<Collider2D>().isTrigger = true;
+        nextFruit.transform.SetParent(player.transform);
+        nextFruit.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 0.2f, player.transform.position.z);
+    }
+
+    void DropCurrentFruit()
+    {
+        nextFruit.transform.position = player.transform.position;
+        nextFruit.GetComponent<Rigidbody2D>().isKinematic = false;
+        nextFruit.GetComponent<Rigidbody2D>().simulated = true;
+        nextFruit.GetComponent<Collider2D>().enabled = true;
+        nextFruit.GetComponent<Collider2D>().isTrigger = false;
+        nextFruit.transform.SetParent(null);
+    }
+
+	// Coroutine to cooldown the player
+    IEnumerator Cooldown()
+    {
+        isCooldown = true;
+        yield return new WaitForSeconds(0.5f);
+        isCooldown = false;
+    } 
 }
